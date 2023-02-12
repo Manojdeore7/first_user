27c2b65dc55750452ddc60bd4e77545a62b6118d
Manojdeore7 <manojddeore11>
2023-02-12 17:21:08 +0530

    Manoj add first

---
+++ first.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j;
+    int arr[23] = {1, 2, 3, 3};
+    // cout << "Enter teh elements:";
+    arr.push(4) for (int i = 0; i < 4; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
